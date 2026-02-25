ac3665fe5545ca3c3e3b0e1c3cbb28ed8110180a
Ohno17 <ajaylego30@gmail.com>
2026-02-25 15:20:11 -0600

    abt pahr
    Co-authored-by: anarayanan1-a11y <anarayanan1-a11y@users.noreply.github.com>

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,6 @@
 
 const header = document.querySelector("header");
+header.className = "docked";
 
 document.addEventListener("scroll", (event) => {
     if (window.scrollY < 1) header.className = "docked";
